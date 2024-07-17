3480147ef4dc1e89152e882e882eab167402fb87
Joom <jnwongsa@gmail.com>
2024-07-17 15:02:16 +0700

    Update: ex1-2

--- ex-1.js
+++ ex-1.js
@@ -1,5 +1,15 @@
 function findStudentIndex(students, searchStudent) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
+
+	for(let i = 0; i < students.length; i++)
+		if(students[i] === searchStudent){
+			return i;
+		}
+	return -1;
+
 }
 
-// ตอบคำถามตรงนี้จ้า
+const students = ["Andrew", "Bobby", "Diana", "George", "Hannah", "Isaac", "Jasmine", "John"] 
+const index = findStudentIndex(students,"John")
+console.log(index);
+
+//*BigO = O(n)linear ทำงานตามจำนวนตัวของinput เข้าไป
